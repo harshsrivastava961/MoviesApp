3c5245c4de85acbf03f6103902726bbd59c38a36
harshsrivastava961 <67214488+harshsrivastava961@users.noreply.github.com>
2022-10-06 20:49:19 +0530

    changes made

---
+++ src/appContainer/DetailsScreenContainer.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const DetailsScreenContainer = () => {
+  return (
+    <View>
+      <Text>DetailsScreenContainer</Text>
+    </View>
+  )
+}
+
+export default DetailsScreenContainer
+
+const styles = StyleSheet.create({})
